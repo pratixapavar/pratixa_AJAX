a2b9bade8753173d70b91b96f4f4f03be3d429c5
pratixa pavar <pratixapavar36@gmail.com>
2023-03-30 13:35:10 +0530

    project completed.. Hurray!!!

---
+++ app1/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+path('', index, name='index'),
+path('create_row/', create_row, name='create_row'),
+path('del_row/',del_row, name = 'del_row')
+]
